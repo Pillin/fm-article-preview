dfb6daed0ec2ae9e8ec7513123cc8104304c4093
Pillippa Ignacia Pérez Pons <ignacia.pp@gmail.com>
2020-06-29 15:43:58 -0400

    added structure

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,10 +2,20 @@ import React from "react";
 import styled from "@emotion/styled";
 import Share from "./Share";
 
-const Container = styled.section``;
-const Img = styled.img``;
+const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  border-radius: 20px;
+  max-width: 320px;
+`;
+const Img = styled.img`
+  max-width: 320px;
+  max-height: 200px;
+`;
 const ImgProfile = styled.img``;
-const UserInformation = styled.section``;
+const UserInformation = styled.section`
+  display: flex;
+`;
 const H4 = styled.h4``;
 const H1 = styled.h1``;
 const Paragraph = styled.p``;
